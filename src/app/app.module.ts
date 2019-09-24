bddc31957bc501a6581dd46da76d65ee3deac86a
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-09-23 22:10:22 -0500

    Creando el componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 // importando el componete Producto
-import { ProductoComponent } from './components/producto/producto.component'
+import { ProductoComponent } from './components/producto/producto.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   // aqui se registran a todos los componentes del modulo APP
   declarations: [
     AppComponent,
-    ProductoComponent
+    ProductoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  marca: string = "CodiGo";
+  sesion: boolean = true;
+  links: Array<string> = ['Nosotres', 'Portafolio', 'Contacto'];
+}
